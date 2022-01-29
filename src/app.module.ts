516004503a5d8678476d3384e195ebc53efa6e7d
1997808 <khanhhuyle22@gmail.com>
2022-01-29 19:02:58 +0700

    connect db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
+import 'dotenv/config'
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(<string>process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      //synchronize: true, // shouldn't be used in production - may lose data
+    }),
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
